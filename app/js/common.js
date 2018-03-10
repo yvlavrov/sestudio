c3b654204c03dc475ffadedd38ac936a11a709b4
yvlavrov <31588604+yvlavrov@users.noreply.github.com>
2018-03-10 11:27:22 +0300

    slider added

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,25 @@
-$(function() {
-
-	// Custom JS
-
+$('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
 });

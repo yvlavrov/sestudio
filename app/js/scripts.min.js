bb50c928969b3ae06bdb01b96a7faf0ee4d0fddd
yvlavrov <31588604+yvlavrov@users.noreply.github.com>
2018-03-11 14:37:57 +0300

    adaptive in progress

--- app/js/scripts.min.js
+++ app/js/scripts.min.js
@@ -33,10 +33,11 @@ function button() {
         btn.style.setProperty('--y', y + 'px')
     }
 }
+
 button();
 
 $(document).ready(function () {
-    $("#menu, #menu2").on("click", "a", function (event) {
+    $("#menu, #mmMenu").on("click", "a", function (event) {
         //отменяем стандартную обработку нажатия по ссылке
         event.preventDefault();
 
@@ -46,7 +47,7 @@ $(document).ready(function () {
             //узнаем высоту от начала страницы до блока на который ссылается якорь
             top = $(id).offset().top;
 
-        //анимируем переход на расстояние - top за 500 мс
+        //анимируем переход на расстояние - top за 1000 мс
         $('body,html').animate({scrollTop: top}, 1000);
     });
 });
@@ -54,6 +55,25 @@ $(document).ready(function () {
 $(function () {
     $('#toTop').click(function () {
         $('body,html').animate({scrollTop: 0}, 800);
-
     });
 });
+
+jQuery(document).ready(function ($) {
+    $("#mmMenu").mmenu({
+        "extensions": [
+            "pagedim-black",
+            "position-right"
+        ],
+        "onClick": {
+            close: true
+        }
+    });
+});
+
+function burgerToggle() {
+    const mmMenu = document.getElementById('mmMenu');
+    const burger = document.querySelector('.hamburger');
+    mmMenu.classList.contains('mm-menu_opened') ? burger.classList.add('is-active') : null
+}
+
+burgerToggle();
